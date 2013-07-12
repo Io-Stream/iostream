7e793ea0e73aaffc1b97638e1d6857d1e578fa48
Londerson <londerson@gmail.com>
2013-07-12 01:13:04 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iostream::Application.config.secret_token = '25acbb672a8bf51eab0c49c9793c208e6c0f18fd1f819d5e04e3184bc1e3b691405f411d09abb21a7e60d91a252d2b2f8a960ca4e1b95b79e46eb201ba687019'
